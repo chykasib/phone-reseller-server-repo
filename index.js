38dcb21e8c61089ee9ff7add68a3e922df33bd0f
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2022-11-25 23:11:50 +0600

    get orders data

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+    });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rx4i6uo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -36,6 +50,26 @@ async function run() {
 
         })
 
+
+        // app.get('/categories', async (req, res) => {
+        //     const query = {}
+        //     const options = await phoneCategoriesCollection.find(query).toArray()
+
+        //     //get the booking of the provided data
+        //     const name = req.query.name;
+        //     const orderQuery = { productName: name };
+        //     const alreadyBooked = await phoneCategoriesCollection.find(orderQuery).toArray();
+
+        //     // code carefully :D
+        //     options.map(option => {
+        //         const optionOrder = alreadyBooked.filter(order => order.name === option.name)
+        //         const bookedSlots = optionOrder.map(book => book.name);
+        //         const remainingSlots = option.products.filter(slot => !bookedSlots.includes(slot))
+        //         option.slots = remainingSlots;
+        //     })
+        //     res.send(options)
+        // })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
@@ -45,6 +79,24 @@ async function run() {
         app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await ordersCollection.insertOne(order);
+            res.send(result)
+        })
+
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            // const decodedEmail = req.decoded.email;
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ message: 'forbidden access' })
+            // }
+            const query = { email: email }
+            const orders = await ordersCollection.find(query).toArray()
+            res.send(orders)
+        });
+
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const order = { _id: ObjectId(id) };
+            const result = await ordersCollection.findOne(booking);
             res.send(result)
         })
 
